4c737228de3351f5a224e89ea39226ccdcdd6315
ChaiChai <1224816105@qq.com>
2019-08-16 17:03:24 +0800

    modify_data_test

--- readdata.py
+++ readdata.py
@@ -6,5 +6,5 @@ f = open('F:\\zhang\\papertest\\naoduxx.csv')
 data = pd.read_csv(f, header=None)
 print(len(data.columns))
 print(len(data))
-dataframe = data.iloc[3114:3138, :9]
+dataframe = data.iloc[3114:3138, :17]
 dataframe.to_csv("test.csv", index=False, index_label=True)
