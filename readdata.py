169d0cf4ae8e8941a03a26bd66a712609ad094a3
ChaiChai <1224816105@qq.com>
2019-08-18 16:54:09 +0800

    x—_data

--- readdata.py
+++ readdata.py
@@ -2,27 +2,36 @@
 import pandas as pd
 from numpy import *
 
-
-f2 = open('F:\\zhang\\papertest\\label.csv')
-y = pd.read_csv(f2, header=None)
-a = []
-for i in range(169):
-    y_i = y.iloc[i]
-    for j in y_i:
-        a.append(j)
-data = pd.DataFrame(a)
-data.to_csv("y.csv", index=False, header=False, mode='a')
-
-# f1 = open('F:\\zhang\\papertest\\data.csv')
+"""
+将label.CSV文件写入y.CSV 二维变一维度
+"""
 # f2 = open('F:\\zhang\\papertest\\label.csv')
-# x = pd.read_csv(f1, header=None)
 # y = pd.read_csv(f2, header=None)
-# ds = 0
-# df = 24
-# for i in range(169):
-#     x_i = x.iloc[ds:df]
-#     x_i.index = [i for i in range(24)]
-#     y_i = y.iloc[i]
-#     y_i.index = [0]
-#     for j in range(18):
-#         x_j = x_i.iloc[,:j]
+# a = []
+# for i in range(169):   # 共169条数据
+#     y_i = y.iloc[i]    #  获取每一行
+#     for j in y_i:
+#         a.append(j)    # 读取每行中元素，生成列表
+# data = pd.DataFrame(a)
+# data.to_csv("y.csv", index=False, header=False, mode='a')  # 写入文件
+
+"""
+
+"""
+f1 = open('F:\\zhang\\papertest\\data.csv')
+x = pd.read_csv(f1, header=None)
+ds = 0
+df = 24
+for i in range(169):
+    x_i = x.iloc[ds:df]
+    for j in range(18):
+        s = x_i.iloc[:, j]
+        a = []
+        a.append(s)
+        d = pd.DataFrame(a)
+        d.to_csv("x.csv", index=False, header=False, mode='a')
+    ds += 24
+    df += 24
+
+
+
